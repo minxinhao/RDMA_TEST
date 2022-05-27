0b4bde61026b25b9aca7fbb1a7809827612b0876
minxinhao <550279741@qq.com>
2022-05-27 15:03:44 +0800

    Fisrt Commit && Able to test send_bw with reasonable results

---
+++ Makefile
@@ -0,0 +1,24 @@
+CC=gcc
+CFLAGS=-O2
+INCLUDES=
+LDFLAGS=-libverbs
+LIBS=-lpthread -lrdmacm -lpmem
+
+SRCS=main.c client.c config.c ib.c server.c setup_ib.c sock.c
+OBJS=$(SRCS:.c=.o)
+PROG=rdma_test
+
+all: $(PROG)
+
+debug: CFLAGS=m -g -DDEBUG
+debug: $(PROG)
+
+.c.o:
+	$(CC) $(CFLAGS) $(INCLUDES) -c -o $@ $<
+
+$(PROG): $(OBJS)
+	$(CC) $(CFLAGS) $(INCLUDES) -o $@ $(OBJS) $(LDFLAGS) $(LIBS)
+	$(RM) *.o
+
+clean:
+	$(RM) *.o *~ $(PROG)
---
+++ main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+#include "debug.h"
+#include "config.h"
+#include "ib.h"
+#include "setup_ib.h"
+#include "client.h"
+#include "server.h"
+
+FILE	*log_fp	     = NULL;
+
+int	init_env    ();
+void	destroy_env ();
+
+int main (int argc, char *argv[])
+{
+    int	ret = 0;
+
+    if (argc != 4) {
+        printf ("Usage: %s Type:[server/client] ip_addre sock_port\n", argv[0]);
+        return 0;
+    }    
+
+    config_info.is_server = !strcmp(argv[1],"server");
+    config_info.ip_address = argv[2];
+    config_info.sock_port = argv[3];
+    config_info.num_concurr_msgs = 1000;
+    config_info.msg_size = 8;
+
+    init_env();
+
+    ret = setup_ib ();
+    check (ret == 0, "Failed to setup IB");
+
+    if (config_info.is_server) {
+        ret = run_server ();
+    } else {
+        ret = run_client ();
+    }
+    check (ret == 0, "Failed to run workload");
+
+ error: 
+    close_ib_connection ();
+    destroy_env         ();
+    return ret;
+}    
+
+int init_env ()
+{
+    char fname[64] = {'\0'};
+
+    if (config_info.is_server) {
+        sprintf (fname, "server.log");
+    } else {
+        sprintf (fname, "client.log");
+    }
+    log_fp = fopen (fname, "w");
+    check (log_fp != NULL, "Failed to open log file");
+
+    log (LOG_HEADER, "IB Echo Server");
+    print_config_info ();
+
+    return 0;
+ error:
+    return -1;
+}
+
+void destroy_env ()
+{
+    log (LOG_HEADER, "Run Finished");
+    if (log_fp != NULL) {
+        fclose (log_fp);
+    }
+}
